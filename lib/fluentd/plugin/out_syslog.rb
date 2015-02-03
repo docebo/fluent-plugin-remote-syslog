ae28b62b575d2c593aa024ca92849322e2f8e7e8
deathowl <csergo.balint@ustream.tv>
2015-02-03 17:42:13 +0100

    TCP support implementation WIP + other improvements.

--- lib/fluentd/plugin/out_syslog.rb
+++ lib/fluentd/plugin/out_syslog.rb
@@ -12,21 +12,36 @@ class SyslogOutput < Fluent::Output
   config_param :hostname, :string, :default => ""
   config_param :remove_tag_prefix, :string, :default => nil
   config_param :tag_key, :string, :default => nil
+  config_param :proto, :default => 'udp' do |val|
+    f = ['tcp', 'udp'].include? val
+    raise ConfigError, "Unsupported protocol '#{val}'" unless f
+    f
+  end
+  config_param :facility, :string, :default => 'user'
+  config_param :severity, :string, :default => 'debug'
+  config_param :use_record, :string, :default => nil
 
-  include Fluent::Mixin::ConfigPlaceholders
 
   def initialize
     super
     require 'socket'
     require 'syslog_protocol'
   end
+
   def configure(conf)
     super
-    @socket = UDPSocket.new
+    if conf['proto'] == 'udp'
+      @socket = UDPSocket.new
+    end
     @packet = SyslogProtocol::Packet.new
     if remove_tag_prefix = conf['remove_tag_prefix']
         @remove_tag_prefix = Regexp.new('^' + Regexp.escape(remove_tag_prefix))
     end
+    @facilty = conf['facility']
+    @severity = conf['severity']
+    @use_record = conf['use_record']
+    @proto = conf['proto']
+    @use_record = conf['use_record']
   end
 
   # This method is called when starting.
@@ -45,17 +60,31 @@ class SyslogOutput < Fluent::Output
   def emit(tag, es, chain)
     chain.next
     es.each {|time,record|
-      @packet.hostname = tag + '-' + hostname
-      @packet.facility = 'user'
-      @packet.severity = 'debug'
+      @packet.hostname = hostname
+      if @use_use_record
+        record['facility'] |= @facilty
+        record['severity'] |= @severity
+        @packet.facility = record['facility']
+        @packet.severity = record['severity']
+      else
+        @packet.facility = @facilty
+        @packet.severity = @severity
+      end
+
       @packet.tag      = if tag_key 
                             record[tag_key][0..31].gsub(/[\[\]]/,'') # tag is trimmed to 32 chars for syslog_protocol gem compatibility
                          else
                             tag[0..31] # tag is trimmed to 32 chars for syslog_protocol gem compatibility
                          end
       packet = @packet.dup
-      packet.content = record['severity'] + ' - ' + record['message']
-      @socket.send(packet.assemble, 0, @remote_syslog, @port)
+      packet.content = record['message']
+      if @proto == 'udp'
+        @socket.send(packet.assemble, 0, @remote_syslog, @port)
+      else
+        sock = TCPSocket.new(@remote_syslog, @port)
+        sock.write packet.assemble + "\n"
+        sock.flush
+      end
 	}
   end
 end
